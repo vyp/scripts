bab1ab5fa7b299ff29377b33d829afac22ba8269
xd1le <elisp.vim@gmail.com>
2017-11-12 21:19:28 +1100

    ♻🔥 lib/guile/scripting: flocks

--- lib/guile/scripting.scm
+++ lib/guile/scripting.scm
@@ -24,23 +24,18 @@
   (system (string-append (editor) " " (string-join files))))
 
 (define-public (mkdir-p . dirs)
-  (map (lambda (dir)
-         (unless (file-exists? dir)
-           (mkdir dir))) dirs))
+  (map (lambda (dir) (unless (file-exists? dir) (mkdir dir)))
+       dirs))
 
 (define-public (enter-dir dir)
   (mkdir-p dir)
   (chdir dir))
 
 (define-public (file-append file text)
-  (let ((port (open-file file "a")))
-    (display text port)
-    (close-port port)))
-
-(define-public (flocks mode . files)
-  (let ((ports (map (lambda (file) (open-file file mode)) files)))
-    (map (lambda (port) (flock port LOCK_EX)) ports)
-    ports))
+  ((lambda (port)
+     (display text port)
+     (close-port port))
+   (open-file file "a")))
 
 (define remove-stat-from-file-system-tree
   (match-lambda
@@ -52,39 +47,30 @@
 (define-public (fs-tree path)
   (remove-stat-from-file-system-tree (file-system-tree path)))
 
-(define-public (get-file ext)
-  (let ((results
-         (scandir "." (lambda (name)
-                        (string-suffix? (string-append "." ext) name)))))
-    (if (null? results)
-        ""
-        (car results))))
-
 (define-public (home-path path)
   (string-append (getenv "HOME") "/" path))
 
 (define-public (read-file file)
-  (let ((lst '()))
-    (with-input-from-file file
-      (lambda ()
-        (do ((line (read-line) (read-line)))
-            ((eof-object? line))
-          (set! lst (cons line lst)))
-        (reverse lst)))))
+  ((lambda (lst) (with-input-from-file file
+              (lambda () (do ((line (read-line) (read-line)))
+                        ((eof-object? line))
+                      (set! lst (cons line lst)))
+                 (reverse lst)))) '()))
 
 (define-public (read-lines port)
-  (let ((lst '()))
-    (do ((line (read-line port) (read-line port)))
-        ((eof-object? line))
-      (set! lst (cons line lst)))
-    (reverse lst)))
+  ((lambda (lst)
+     (do ((line (read-line port) (read-line port)))
+         ((eof-object? line))
+       (set! lst (cons line lst)))
+     (reverse lst)) '()))
 
+;; I'm not sure if this actually works, shouldn't `read-lines' take a port, not
+;; a file[name]?
 (define-public (sort-file file)
-  (let ((contents (read-lines file)))
-    (call-with-output-file file
-      (lambda (out)
-        (display (string-join
-                  (list-sort string<? contents) "\n" 'suffix) out)))))
+  (call-with-output-file file
+    (lambda (out) (display (-> (read-lines file)
+                          (lambda (ls) (list-sort string<? ls))
+                          (string-join "\n" 'suffix)) out))))
 
 (define-public (system-output command)
   (let* ((port (open-input-pipe command))
@@ -94,9 +80,9 @@
         ""
         output)))
 
-(define-public (touch . files)
-  (map (lambda (file)
-         (unless (file-exists? file)
-           (let ((port (open-file file "w")))
-             (display "" port)
-             (close-port port)))) files))
+(define-public (touch-files . files)
+  (map (lambda (file) (unless (file-exists? file)
+                         ((lambda (port)
+                            (display "" port)
+                            (close-port port))
+                          (open-file file "w"))) files)))
