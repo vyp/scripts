e887604611a1f75ac3e664e8d5ed0b2585a79a9f
xd1le <elisp.vim@gmail.com>
2017-11-27 23:59:36 +1100

    📝 lib/guile/alist-utils: simplify

--- lib/guile/alist-utils.scm
+++ lib/guile/alist-utils.scm
@@ -3,7 +3,6 @@
 
 ;; Quite useless to use compared to just acons, as assoc and assoc-ref return
 ;; the first pair from pairs with the same key anyway, but using this does
-;; remove stateful cruft, which becomes useful for example when storing data to
-;; disk.
+;; remove stateful cruft, which is useful for example when storing data to disk.
 (define-public (assoc-replace key value alist)
   (acons key value (alist-delete key alist)))
